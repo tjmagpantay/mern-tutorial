d1dbeb1c2effe09daaaf4f19ac80c92d6feebcce
Tj <tjmagpantay23@gmail.com>
2025-03-29 22:05:45 +0800

    Fixed embedded repository issue in server folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { BrowseRouter, Route, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
