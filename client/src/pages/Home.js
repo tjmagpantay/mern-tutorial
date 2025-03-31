836d70db6f9bb4eb6f535eb3d30be26fbf8f15a9
Tj <tjmagpantay23@gmail.com>
2025-03-31 22:12:52 +0800

    update - need to fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,29 @@
+import { useEffect , useState} from "react";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  // Fetch workouts from the API when the component mounts
+
+useEffect(() => {
+  const fetchWorkouts = async () => {
+    const response = await fetch("/api/workouts");
+    const json = await response.json();
+    if (response.ok) {
+      setWorkouts(json);
+    }
+  }
+
+  fetchWorkouts();
+}, []);
+
   return (
     <div className="home">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <p key={workout._id}>{workout.title}</p>
+        ))}
+      </div>
     </div>
   );
 }
